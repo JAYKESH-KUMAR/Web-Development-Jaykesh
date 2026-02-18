edc90841b9ba0a3e8d81e2797f325f5d53d9f263
Jaykesh Kumar <jaykeshkumar549@gmail.com>
2026-02-18 10:38:51 +0530

    video71 javascript in backend

---
+++ video71/server.js
@@ -0,0 +1,11 @@
+//console.log("Hello world")
+
+
+var slugify= require('slugify')
+let a= slugify('some string')
+console.log(a)
+
+// if you prefer something other than  '-' as separator
+
+const b=slugify('some st&&*(^%$$^^&ring', '-')   // some_string
+console.log(b) 
