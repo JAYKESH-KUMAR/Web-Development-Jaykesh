5a7cc01a262c3cc29bd30d6f9ade3a80c0d896b1
Jaykesh Kumar <jaykeshkumar549@gmail.com>
2026-02-18 20:53:07 +0530

    video72 javascript

---
+++ video72/main.js
@@ -0,0 +1,26 @@
+//const http= require('node:http');
+//const fs= require("fs")
+//import http from "http"
+//const hostname = '127.0.0.1';
+//const port = 3000;
+//const server = createServer((req, res) => {
+  //res.statusCode = 200;
+  //res.setHeader('Content-Type', 'text/html');
+ // res.end('<h1> Hello World</h1>');
+//});
+//server.listen(port, hostname, () => {
+ // console.log(`Server running at http://${hostname}:${port}/`);
+//});
+
+//import {a, b, d} from "./mymodule.js"
+//console.log(a,b,d)
+
+//import obj from "./mymodule.js"
+//console.log(obj)
+// (function(export,requre,module, _filename, _dirname){
+// Module code actually lives here
+//});
+
+const a = require("./mymodule2.js")
+
+console.log(a, __dirname, _filename)
